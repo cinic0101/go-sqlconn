012957a1d00810e57d6731887c878c8a75033a9a
Derek <chuanfeng.wu@pousheng.com>
2018-11-09 14:29:30 +0800

    First commit

---
+++ config.go
@@ -0,0 +1,34 @@
+package go_sqlconn
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DBConfig struct {
+	Databases map[string]Database `yaml:"databases"`
+}
+
+type Database struct {
+	Driver string
+	Host string
+	Database string
+	User string
+	Password string
+}
+
+func UnmarshalConfig(filename string) (*DBConfig, error) {
+	c := &DBConfig{}
+
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = yaml.Unmarshal(buf, &c); err != nil {
+		return nil, err
+	}
+
+	return c, err
+}
---
+++ sqlconn.go
@@ -0,0 +1,61 @@
+package go_sqlconn
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+)
+
+type Databases struct {}
+
+type DBConn struct {
+	Driver string
+	DataSource string
+}
+
+var once sync.Once
+var dbInstances map[string]*DBConn
+
+func (d *Databases) NewInstance(db string) *DBConn {
+	once.Do(func() {
+		c, err := UnmarshalConfig(os.Getenv("CONFIG"))
+		if err != nil {
+			panic(err)
+		}
+
+		dbInstances =  make(map[string]*DBConn, len(c.Databases))
+		for k, v := range c.Databases {
+			dbInstances[strings.ToLower(k)] = &DBConn{
+				Driver: v.Driver,
+				DataSource: fmtDataSource(v),
+			}
+		}
+	})
+
+	return dbInstances[strings.ToLower(db)]
+}
+
+func (d *DBConn) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	db, err := sql.Open(d.Driver, d.DataSource)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	return db.Query(query, args...)
+}
+
+func fmtDataSource(db Database) string {
+	switch db.Driver {
+	case "mysql":
+		return fmt.Sprintf("%s:%s@tcp(%s)/%s",
+			db.User, db.Password, db.Host, db.Database)
+	case "sqlserver":
+		return fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+			db.Host, db.User, db.Password, 1433, db.Database)
+	default:
+		return fmt.Sprintf("Unknown driver: %s", db.Driver)
+	}
+}
